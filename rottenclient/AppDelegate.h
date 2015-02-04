6536bd3d3901ecffde4509099b4cf6add54978bd
Naeim Semsarilar <nsemsarilar@box.com>
2015-02-03 22:49:52 -0800

    Initial Commit

---
+++ rottenclient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenclient
+//
+//  Created by Naeim Semsarilar on 2/3/15.
+//  Copyright (c) 2015 naeim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottenclient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rottenclient
+//
+//  Created by Naeim Semsarilar on 2/3/15.
+//  Copyright (c) 2015 naeim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
